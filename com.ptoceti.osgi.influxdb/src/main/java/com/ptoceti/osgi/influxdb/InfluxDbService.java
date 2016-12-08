c5f67fba0fb35e05be192d4be44218f03e75558c
lathik <lathil.dev@gmail.com>
2016-12-08 13:01:25 +0100

    Removed influxdb project. Create separate Influxdb-fluentapi project on Github

--- com.ptoceti.osgi.influxdb/src/main/java/com/ptoceti/osgi/influxdb/InfluxDbService.java
+++
@@ -1,6 +0,0 @@
-package com.ptoceti.osgi.influxdb;
-
-public interface InfluxDbService {
-
-    void writePoint(Point point);
-}
--- com.ptoceti.osgi.influxdb/src/main/java/com/ptoceti/osgi/influxdb/ql/Identifier.java
+++
@@ -1,5 +0,0 @@
-package com.ptoceti.osgi.influxdb.ql;
-
-public class Identifier {
-
-}
