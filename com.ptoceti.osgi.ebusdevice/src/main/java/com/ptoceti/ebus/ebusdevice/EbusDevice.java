11fd7914ea2d6ccc9988fbbad896ac72f38802f6
lthil <laurent.thil@gmail.com>
2020-02-08 16:29:12 +0100

    Ajout EbusDevice

---
+++ com.ptoceti.osgi.ebusdevice/src/main/java/com/ptoceti/ebus/ebusdevice/EbusDevice.java
@@ -0,0 +1,20 @@
+package com.ptoceti.ebus.ebusdevice;
+
+import org.osgi.service.wireadmin.Consumer;
+import org.osgi.service.wireadmin.Producer;
+
+public interface EbusDevice extends Producer, Consumer {
+    /**
+     * Return the id of the device in the Modbus bus link
+     * @return the id of this device
+     */
+    public int getId();
+
+    /**
+     * Return the serial link port used by the device.
+     * @return String the name of the port for this device
+     */
+    public String getPortName();
+
+    public void stop();
+}
