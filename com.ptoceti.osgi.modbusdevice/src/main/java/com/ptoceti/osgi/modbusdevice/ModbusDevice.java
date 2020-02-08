536685cdd4b1dd288be2944f1ef8e04d8b048699
lthil <laurent.thil@gmail.com>
2020-02-08 16:12:27 +0100

    Update Ebus device

--- com.ptoceti.osgi.modbusdevice/src/main/java/com/ptoceti/osgi/modbusdevice/ModbusDevice.java
+++ com.ptoceti.osgi.modbusdevice/src/main/java/com/ptoceti/osgi/modbusdevice/ModbusDevice.java
@@ -23,7 +23,8 @@ package com.ptoceti.osgi.modbusdevice;
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * #L%
+
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * #L%
